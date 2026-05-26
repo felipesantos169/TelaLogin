045404d99ea8208fd4521d27030e955d2ee1476f
Felipe <felipe_santos169@estudante.sesisenai.org.br>
2026-05-26 20:31:26 -0300

    cadastro finalizado

--- cadastro.js
+++ cadastro.js
@@ -2,85 +2,84 @@ const inputEmail = document.getElementById('inputEmail')
 const inputSenha = document.getElementById('inputSenha')
 const inputCpf = document.getElementById('inputCpf')
 const inputTelefone = document.getElementById('inputTel')
-const feedback = document.getElementById('feedback')
 const form = document.getElementById('form')
 
 const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
-const regexCPF = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+const regexCpf = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
 
 const regexTelefone = /^\(\d{2}\)\s\d{4,5}-\d{4}$/
 
 form.addEventListener('submit', e => {
     e.preventDefault()
     const email = inputEmail.value
-    const senha = inputSenha
-    const cpf = inputCpf
-    const telefone = inputTelefone
-    validarEmail(email)
-    validarSenha(senha)
-    validarCpf(cpf)
-    validarTelefone(telefone)
+    const senha = inputSenha.value
+    const cpf = inputCpf.value
+    const telefone = inputTelefone.value
+    validarEmail(email, inputEmail)
+    validarSenha(senha, inputSenha)
+    validarCpf(cpf, inputCpf)
+    validarTelefone(telefone, inputTelefone)
 })
 
 function limparErro(informacao){
     informacao.textContent = ''
 }
 
-function validarEmail(email) {
-    if(!regexEmail(email)){
-        console.log("Erro! Email Inválido")
-        feedback.classlist.add('errado')
+function validarEmail(email, inputEmail) {
+    if(!regexEmail.test(email)){
+        alert("Erro! Email Inválido")
+        inputEmail.classList.remove('correto')
+        inputEmail.classList.add('errado')
         return false;
     }
     else {
-        limparErro(email)
-        feedback.classlist.add('correto')
-        console.log('Válido')
+        limparErro(inputEmail)
+        inputEmail.classList.add('correto')
         return true
     }
 }
 
-function validarSenha(senha) {
+function validarSenha(senha, inputSenha) {
     const minLength = 6
     tamanho = senha.length
     if(tamanho < minLength){
-        console.log("Erro! Minímo 6 caracteres")
-        feedback.classlist.add('errado')
+        alert("Erro! Minímo 6 caracteres na senha")
+        inputSenha.classList.remove('correto')
+        inputSenha.classList.add('errado')
         return false;
     }
     else {
-        limparErro(senha)
-        feedback.classlist.add('correto')
-        console.log('Válida')
+        limparErro(inputSenha)
+        inputSenha.classList.add('correto')
         return true
     }
 }
 
-function validarCpf(cpf) {
-    if(!regexCpf(cpf)){
-        console.log("Erro! CPF Inválido")
-        feedback.classlist.add('errado')
+function validarCpf(cpf, inputCpf) {
+    if(!regexCpf.test(cpf)){
+        alert("Erro! CPF Inválido")
+        inputCpf.classList.remove('correto')
+        inputCpf.classList.add('errado')
         return false;
     }
     else {
-        limparErro(cpf)
-        feedback.classlist.add('correto')
-        console.log('Válido')
+        limparErro(inputCpf)
+        inputCpf.classList.add('correto')
         return true
     }
 }
 
-function validarTelefone(telefone) {
-    if(!regexTelefone(telefone)){
-        console.log("Erro! Telefone Inválido")
-        feedback.classlist.add('errado')
+function validarTelefone(telefone, inputTelefone) {
+    if(!regexTelefone.test(telefone)){
+        alert("Erro! Telefone Inválido")
+        inputTelefone.classList.remove('correto')
+        inputTelefone.classList.add('errado')
         return false;
     }
     else {
-        limparErro(telefone)
-        feedback.classlist.add('correto')
-        console.log('Válido')
+        limparErro(inputTelefone)
+        inputTelefone.classList.add('correto')
         return true
     }
 }
